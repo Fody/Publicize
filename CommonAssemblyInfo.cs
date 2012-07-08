0ca78fb08ef1c53023cab7b05ef07ad3da41d1d6
Simon Cropp <simon.cropp@gmail.com>
2012-07-08 15:01:54 +1000

    bump version

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -4,5 +4,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Publicize")]
 [assembly: AssemblyProduct("Publicize")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.8.0.0")]
-[assembly: AssemblyFileVersion("0.8.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
