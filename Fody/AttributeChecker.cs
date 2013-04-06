e211bb7ce7c3dcd600d2e66869e1f42d6dfafecc
Simon Cropp <simon.cropp@gmail.com>
2013-04-06 22:45:48 +1100

    use scalpel

--- Fody/AttributeChecker.cs
+++ Fody/AttributeChecker.cs
@@ -1,7 +1,9 @@
 using System.ComponentModel;
 using System.Linq;
 using System.Reflection;
+using Scalpel;
 
+[Remove]
 public static class AttributeChecker
 {
     public static bool ContainsHideAttribute(this ICustomAttributeProvider provider)
