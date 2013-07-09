7ddefa4bb586b1bf107018208c258a8f3898c193
Simon Cropp <simon.cropp@gmail.com>
2013-07-09 22:27:15 +1000

    fix VS Guid

--- CommonAssemblyInfo.cs
+++ CommonAssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Publicize")]
 [assembly: AssemblyProduct("Publicize")]
-[assembly: AssemblyVersion("1.3.3")]
-[assembly: AssemblyFileVersion("1.3.3")]
+[assembly: AssemblyVersion("1.3.4")]
+[assembly: AssemblyFileVersion("1.3.4")]
